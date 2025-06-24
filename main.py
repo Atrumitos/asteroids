15d4027a623716ee15dbd4a3008a0669ca1f5924
Atrumitos <aris.g.g@outlook.com>
2025-06-24 23:59:58 +0300

    B: Player ready

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
 from constants import *
+from circleshape import *
+from player import *
 
 def main():
     pygame.init()
@@ -8,12 +10,24 @@ def main():
     print("Screen height: 720")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     surface = pygame.Surface((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    x=SCREEN_WIDTH/2
+    y=SCREEN_HEIGHT/2
+    Player.containers = (updatable, drawable)
+    player_character=Player(x, y)
+    dt=16/1000
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        surface.fill((0,0,0), rect=None, special_flags=0)
+        screen.fill((0,0,0), rect=None, special_flags=0)
+        updatable.update(dt)
+        for d in drawable:
+            d.draw(screen)
         pygame.display.flip()
+        clock.tick(60)
 
 if __name__ == "__main__":
     main()
